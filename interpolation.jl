938e373c8a9d973ced899c91823ad2667755de45
unknown <>
2016-05-09 18:53:31 +0900

    text editer

---
+++ interpolation.jl
@@ -0,0 +1,14 @@
+function my_lin_interp(grid, vals)
+    function func(x)
+        index_1 = searchsortedfirst(grid, x)
+        index_0 = searchsortedlast(grid, x)
+        x_1 = grid[index_1]
+        x_0 = grid[index_0]
+        y_1 = vals[index_1]
+        y_0 = vals[index_0]
+        
+        y = y_0 + (y_1 - y_0) * ((x - x_0) / (x_1 - x_0))
+        return y
+    end
+    return func
+end
