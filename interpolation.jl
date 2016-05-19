3c58eaa8fe7db88e31d309342dc9b65c8b230c33
unknown <>
2016-05-19 15:05:15 +0900

    2と3の順番入れ替え

--- interpolation.jl
+++ interpolation.jl
@@ -1,7 +1,7 @@
 function my_lin_interp(grid, vals)
     function func(x)
-        index_1 = index_0 + 1
         index_0 = searchsortedlast(grid, x)
+        index_1 = index_0 + 1
         x_1 = grid[index_1]
         x_0 = grid[index_0]
         y_1 = vals[index_1]
